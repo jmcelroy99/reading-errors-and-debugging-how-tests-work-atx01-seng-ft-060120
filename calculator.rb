a7123498733558a07af8e77861740cf56f105a7a
energetic-commit-5967 <jmcelroy@plexustech.net>
2020-04-17 12:33:01 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -6,3 +6,9 @@ second_number = 3
 
 
 sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = second_number / first_number
